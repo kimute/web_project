398884b57e027da01ab7cfd0ca37b37a1f1d4481
キム・テフン <kimutefun@kimutefunnoMacBook-Pro.local>
2019-06-10 00:20:03 +0900

    rest apiを作成

---
+++ api_blog/views.py
@@ -0,0 +1,22 @@
+import django_filters
+from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import User, Entry
+from .serializer import UserSerializer, EntrySerializer
+
+
+'''
+Parameter
+------------------
+queryset:Djangoのmodelのクエリセットを指定
+serializer_class:serializerに定義したserializerを指定
+'''
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class EntryViewSet(viewsets.ModelViewSet):
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
